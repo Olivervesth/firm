22815295ac98d4d756f2a51162270de17a6d2d47
Olivervesth <48321388+Olivervesth@users.noreply.github.com>
2021-08-28 08:55:17 +0200

    Add files via upload

---
+++ Firm/Firm/FirmContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using System.Threading.Tasks;
+
+namespace Firm
+{
+    class FirmContext: DbContext
+    {       
+        public FirmContext() : base("fDb")
+        {
+            Database.SetInitializer<FirmContext>(new DropCreateDatabaseAlways<FirmContext>()); // Make sure we dont save any data when the application is not running
+        }
+
+        public DbSet<Worker> DbWorker { get; set; }
+        public DbSet<Department> DbDepartment { get; set; }
+    }
+}
